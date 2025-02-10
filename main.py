f0319c69207c220d2fbd402103618836ef1a8551
luminousseelie <luminousseelie@gmail.com>
2025-02-10 14:29:32 +0800

    Changed main.py to count all symbols in frankenstein.txt

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    words = file_contents.split()
-    print(str(len(words)))
+    book_path = "books/frankenstein.txt"
+    text = str(get_book_text(book_path)).lower()
+    symbol_no = count_symbols(text)
+    print(symbol_no)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_symbols(string):
+    symbol_dict = {}
+    for i in range(len(string) - 1):
+        symbol_dict[string[i]] = symbol_dict.get(string[i], 0) + 1
+    return symbol_dict
 
 main()
