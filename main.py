60b9b9caf643ea8337816a6bfe8ba745a8d51711
luminousseelie <luminousseelie@gmail.com>
2025-02-10 14:46:28 +0800

    Made a report for symbols counted.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     book_path = "books/frankenstein.txt"
     text = str(get_book_text(book_path)).lower()
     symbol_no = count_symbols(text)
-    print(symbol_no)
+    report_symbols(symbol_no)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,7 +11,13 @@ def get_book_text(path):
 def count_symbols(string):
     symbol_dict = {}
     for i in range(len(string) - 1):
-        symbol_dict[string[i]] = symbol_dict.get(string[i], 0) + 1
+        symbol_dict[str(string[i])] = symbol_dict.get(string[i], 0) + 1
     return symbol_dict
+
+def report_symbols(dict):
+    keys = list(dict.keys())
+    values = list(dict.values())
+    for i in range(len(keys) - 1):
+        print("The letter '" + keys[i] + "' was found " + str(values[i]) + " times.")
 
 main()
